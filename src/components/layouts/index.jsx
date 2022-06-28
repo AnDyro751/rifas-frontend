1138a89ce677043526b62a144c7638edad3157a9
Angel Mendez <amendez@whiteboard.cl>
2022-06-28 15:00:13 -0500

    Title

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,10 +1,16 @@
 import Header from '../header'
 import Footer from '../footer'
+import { Head } from 'next/document'
 
 const MainLayout = ({ children }) => {
 
   return (
     <div>
+      <Head>
+        <title>
+          Rifas Tomin
+        </title>
+      </Head>
       <Header />
       <main className="w-full flex justify-center flex-wrap">
         {children}
